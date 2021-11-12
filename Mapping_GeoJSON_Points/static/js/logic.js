211304f2753097e1969f042acf4ce5a575dd40ea
Matt Lane <matthewlane412@yahoo.com>
2021-11-12 17:02:31 -0600

    13.5.2

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,7 +2,50 @@
 console.log("working");
 
 // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([38.6270, -90.1994], 4);
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// Grabbing our GeoJSON data.
+    L.geoJson(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+        onEachFeature: function(feature, layer) {
+            console.log(layer);
+            layer.bindPopup("<h3> Airport code: " + feature.properties.faa + "</h3><hr><h3> Airport name: " + feature.properties.name +"</h3>");
+        }
+    }).addTo(map);
+
+        // // Grabbing our GeoJSON data.
+        // L.geoJson(sanFranAirport, {
+        //     // We turn each feature into a marker on the map.
+        //     pointToLayer: function(feature, latlng) {
+        //       console.log(feature);
+        //       return L.marker(latlng)
+        //       .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>")
+        //     }
+        //   }).addTo(map);
+
+
+
+
 
 // // Coordinates for each point to be used in the polyline.
 // let line = [
@@ -53,26 +96,26 @@ L.polyline(line, {
 //  }).addTo(map);
 
 // Get data from cities.js
-let cityData = cities;
+    // let cityData = cities;
 
 
-// // Loop through the cities array and create one marker for each city.
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     L.marker(city.location)
-//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//   .addTo(map);
-// });
-   
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+    // // // Loop through the cities array and create one marker for each city.
+    // // cityData.forEach(function(city) {
+    // //     console.log(city)
+    // //     L.marker(city.location)
+    // //     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    // //   .addTo(map);
+    // // });
+    
+    // // Loop through the cities array and create one marker for each city.
+    // cityData.forEach(function(city) {
+    //     console.log(city)
+    //     L.circleMarker(city.location, {
+    //         radius: city.population/100000
+    //     })
+    //     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    //   .addTo(map);
+    // });
 
 // // We create the tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -88,7 +131,7 @@ cityData.forEach(function(city) {
 // });
 
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
